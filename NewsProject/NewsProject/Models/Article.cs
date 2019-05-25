92810c63dbdb9895f1a8d40545e9c2ca580be817
omer nahum <omernah93@gmail.com>
2019-05-25 16:14:35 +0300

    Aritcle Class

---
+++ NewsProject/NewsProject/Models/Article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+public class Article
+{
+    public int ArticleId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Date { get; set; }
+    public int NumOfLikes { get; set; }
+    public List<User> Users { get; set; }
+    public string Image { get; set; }
+    public string ArticleLink { get; set; }
+    public User Writer { get; set; }
+    
+}
