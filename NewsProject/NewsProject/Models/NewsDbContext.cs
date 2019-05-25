3a5b383b0ed6bb4602bd40c5faee3c7e803086cf
Or Mizrahi <315961052@STUD.colman.ac.il>
2019-05-25 16:53:53 +0300

    update

---
+++ NewsProject/NewsProject/Models/NewsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace NewsProject.Models
+{
+    public class NewsDbContext : DbContext
+    {
+         public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
