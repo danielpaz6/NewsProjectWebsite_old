48b02d3eaf647c376cf704a5b7f38397a4d10748
omer nahum <omernah93@gmail.com>
2019-05-25 16:37:40 +0300

    Article Class

--- NewsProject/NewsProject/Models/Article.cs
+++ NewsProject/NewsProject/Models/Article.cs
@@ -7,5 +7,15 @@ namespace NewsProject.Models
 {
     public class Article
     {
+        public int ArticleId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+        public int NumOfLikes { get; set; }
+        public List<User> Users { get; set; }
+        public string Image { get; set; }
+        public string ArticleLink { get; set; }
+        public User Writer { get; set; }
+
     }
 }
