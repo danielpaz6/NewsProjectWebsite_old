88a7192dcf99dfa3919372a6d70e1aece25143e1
omer nahum <omernah93@gmail.com>
2019-05-25 16:22:08 +0300

    count deleted

--- NewsProject/NewsProject/Models/Category.cs
+++ NewsProject/NewsProject/Models/Category.cs
@@ -10,6 +10,6 @@ namespace NewsProject.Models
         public int CategoryId { get; set; }
         public string Color { get; set; }
         public string Name { get; set; }
-        public int count { get; set; }
+        
     }
 }
