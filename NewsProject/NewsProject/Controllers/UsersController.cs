8157c7e21838b0f75540d4a81262f650e347ac65
Daniel Paz <danielpaz601@gmail.com>
2019-05-25 16:56:40 +0300

    Fixed the UsersController.cs

--- NewsProject/NewsProject/Controllers/UsersController.cs
+++ NewsProject/NewsProject/Controllers/UsersController.cs
@@ -17,7 +17,7 @@ namespace NewsProject.Controllers
         // GET: Users
         public ActionResult Index()
         {
-            return View(db.users.ToList());
+            return View(db.Users.ToList());
         }
 
         // GET: Users/Details/5
@@ -27,7 +27,7 @@ namespace NewsProject.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            User user = db.users.Find(id);
+            User user = db.Users.Find(id);
             if (user == null)
             {
                 return HttpNotFound();
@@ -50,7 +50,7 @@ namespace NewsProject.Controllers
         {
             if (ModelState.IsValid)
             {
-                db.users.Add(user);
+                db.Users.Add(user);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
@@ -65,7 +65,7 @@ namespace NewsProject.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            User user = db.users.Find(id);
+            User user = db.Users.Find(id);
             if (user == null)
             {
                 return HttpNotFound();
@@ -96,7 +96,7 @@ namespace NewsProject.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            User user = db.users.Find(id);
+            User user = db.Users.Find(id);
             if (user == null)
             {
                 return HttpNotFound();
@@ -109,8 +109,8 @@ namespace NewsProject.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            User user = db.users.Find(id);
-            db.users.Remove(user);
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
