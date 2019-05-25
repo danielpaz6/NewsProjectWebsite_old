ee8a65dc075e80263356aed71a2f57e3a1100ce9
opal5993 <opal5993@gmail.com>
2019-05-25 16:37:54 +0300

    User Class

--- NewsProject/NewsProject/Models/User.cs
+++ NewsProject/NewsProject/Models/User.cs
@@ -7,5 +7,11 @@ namespace NewsProject.Models
 {
     public class User
     {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public int UserPassword { get; set; }
+        public string Email { get; set; }
+        public int Permission { get; set; }
     }
+
 }
