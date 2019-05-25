136568c9f4ab5cae0989672ab4d34871affbc99c
Daniel Paz <danielpaz601@gmail.com>
2019-05-25 14:44:58 +0300

    initalized files.

---
+++ NewsProject/NewsProject/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace NewsProject
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
