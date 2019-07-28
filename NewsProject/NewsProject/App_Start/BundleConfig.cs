c58e356fb1e39181dc5f64fc487a8f3f30151551
Daniel Paz <danielpaz601@gmail.com>
2019-07-28 14:42:41 +0300

    Updated the javascript locations

--- NewsProject/NewsProject/App_Start/BundleConfig.cs
+++ NewsProject/NewsProject/App_Start/BundleConfig.cs
@@ -9,7 +9,10 @@ namespace NewsProject
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/script").Include(
+                        "~/Scripts/script.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -20,7 +23,7 @@ namespace NewsProject
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       //"~/Content/bootstrap.css",
